721f06ea2520da3485f142bafdbcd464e4da69db
Tim Lupo <socialmedia@timlupo.com>
2016-01-23 10:41:01 -0500

    Updated Mentors, Team, and Partners

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,9 @@
-//var mentors = [["Tim Lupo", 41.057319, -74.140977], ["Neel Patel-Shah", 40.940376, -74.131810], ["Joy Hsu", 33.816960, -118.037285]];
-var students = [[41.057319, -74.140977], [37.548270, -121.988572], [40.006496, -75.703274], [36.974117, -122.030796]];
+var mentors = [["Tim Lupo", 41.057319, -74.140977], ["Yasmeen Roumie", 40.712784, -74.005941], ["Caitlin Stanton", 40.712784, -74.005941]];
+//var students = [[41.057319, -74.140977], [37.548270, -121.988572], [40.006496, -75.703274], [36.974117, -122.030796]];
 
 function initialize() {
   var mapProp = {
-    center:new google.maps.LatLng(38, -95),
+    center:new google.maps.LatLng(38, -90),
     zoom:4,
     scrollwheel: false,
     mapTypeId:google.maps.MapTypeId.ROADMAP
@@ -12,21 +12,20 @@ function initialize() {
     
 //var marker = new google.maps.Marker({ position: new google.maps.LatLng(41.0573, -74.1410), map: map ,icon: 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'});
     
-    for (var i = 0; i < students.length; i++) {
+    /*for (var i = 0; i < students.length; i++) {
         var student = students[i];
         var marker = new google.maps.Marker({
           position: {lat: student[0], lng: student[1]},
           map: map
         });
-    }
+    }*/
     
-    /*for (var i = 0; i < mentors.length; i++) {
+    for (var i = 0; i < mentors.length; i++) {
         var mentor = mentors[i];
         var marker = new google.maps.Marker({
           position: {lat: mentor[1], lng: mentor[2]},
-          map: map,
-          icon: 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'
+          map: map
         });
-    }*/
+    }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
