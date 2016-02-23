017c6a45e8bf37c49841e74f07d3b6d1c0d06a80
Tim Lupo <socialmedia@timlupo.com>
2016-02-23 09:39:17 -0500

    Added Summer Program, Updated Advisors, and Minor Bug Fixes

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,8 +1,14 @@
-$('.carousel').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  centerMode: true,
-  arrows: false
+$(document).ready(function(){
+    $('.projectswitch').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      accessibility: true,
+      infinite: true,
+      pauseOnHover: false,
+      swipe: true,
+      swipeToSlide: true,
+      arrows: false
+    });
 });
